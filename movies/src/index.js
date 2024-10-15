290401dc1f0d2e4f28499aaf4320fbf7b51d7ec2
igorkapusniak0 <20102236@mail.wit.ie>
2024-10-15 10:18:12 +0100

    movie details coming from api

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
-import MovieDetailsPage from './pages/movieDetailsPage'
+import MoviePage from "./pages/movieDetailsPage";
 
 
 const images = [
@@ -96,8 +97,14 @@ const movies = [sample, sample, sample, sample, sample, sample, sample];
 
 const App = () => {
   return (
-      <MovieDetailsPage movie={sample} images={images} />
-      );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/movies/:id" element={<MoviePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={ <Navigate to="/" /> } />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 const rootElement = createRoot( document.getElementById("root") )
